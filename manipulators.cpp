386d9c53814c00e0b90cdee697384acbc25129db
Krishangi verma <krishangiverma52@gmail.com>
2026-02-07 12:41:48 +0530

    Added a c++ program of manipulators

---
+++ manipulators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a=3, b=78,c=1233,d=34567;
+    cout<<"value of a is: "<<setw(5)<<a<<endl;
+    cout<<"value of b is: "<<setw(5)<<b<<endl;
+    cout<<"value of c is: "<<setw(5)<<c<<endl;
+    cout<<"value of d is: "<<setw(5)<<d<<endl;
+    return 0;
+}
